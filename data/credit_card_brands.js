be36222fc1faf2279fbb7a457d89ab1b19f80b1a
Philip Bergqvist <philip@pqvst.com>
2019-08-02 11:37:10 +0200

    Refactoring data

---
+++ data/credit_card_brands.js
@@ -0,0 +1,10 @@
+module.exports = {
+  amex: 'American Express',
+  diners: 'Diners Club',
+  discover: 'Discover',
+  jcb: 'JCB',
+  mastercard: 'MasterCard',
+  unionpay: 'UnionPay',
+  visa: 'Visa',
+  unknown: 'Unknown',
+};
---
+++ data/month_names.js
@@ -0,0 +1,14 @@
+module.exports = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
