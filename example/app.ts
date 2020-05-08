1fb68b54bc54679efcc0b9d16ad0eff54986a5c9
Philip Bergqvist <philip@pqvst.com>
2020-05-08 14:57:17 +0800

    Updating example app

--- example/app.ts
+++ example/app.ts
@@ -7,23 +7,23 @@ import dotenv from 'dotenv';
 
 import Checkout from '../lib';
 
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const STRIPE_PUBLIC_KEY = process.env.STRIPE_PUBLIC_KEY;
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
-const PREMIUM_PLAN_ID = process.env.PREMIUM_PLAN_ID;
+const PLAN_ID = process.env.PLAN_ID;
 const PORT = process.env.PORT || 3000;
 
 
 if (!STRIPE_PUBLIC_KEY || !STRIPE_SECRET_KEY) {
   throw new Error('You must add your stripe public and secret key.');
 }
-if (!PREMIUM_PLAN_ID) {
-  throw new Error('You must define a premium plan id to be able to test upgrades.');
+if (!PLAN_ID) {
+  throw new Error('You must define a plan id for testing purposes.');
 }
 
 
-const checkout = Checkout(new Stripe(STRIPE_SECRET_KEY, null));
+const checkout = new Checkout(new Stripe(STRIPE_SECRET_KEY, null));
 
 // Setup express
 const app = express();
@@ -31,12 +31,12 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', __dirname);
 app.set('view engine', 'pug');
-app.use('/js/checkout.js', express.static(path.join(__dirname, '../dist/checkout.js')));
+app.use('/js/checkout.js', express.static(path.join(__dirname, '../dist/client/checkout.js')));
 
 
 // Implement endpoint for VAT number validation
 app.get('/validateVatNumber', (req, res) => {
-  checkout.validateVatNumber(req.query.q).then(valid => {
+  checkout.validateVatNumber(req.query.q as string).then(valid => {
     res.status(valid ? 200 : 400).json({ valid });
   });
 });
@@ -44,7 +44,7 @@ app.get('/validateVatNumber', (req, res) => {
 
 // Implement endpoint for coupon validation
 app.get('/validateCoupon', (req, res) => {
-  checkout.validateCoupon(req.query.q).then(valid => {
+  checkout.validateCoupon(req.query.q as string).then(valid => {
     res.status(valid ? 200 : 400).json({ valid });
   });
 });
@@ -54,15 +54,15 @@ app.get('/validateCoupon', (req, res) => {
 app.get('/', async (req, res) => {
   res.render('index', {
     customer: req.query.customer || '',
-    sub: await checkout.getSubscription(req.query.customer),
-    receipts: await checkout.getReceipts(req.query.customer),
+    sub: await checkout.getSubscription(req.query.customer as string),
+    receipts: await checkout.getReceipts(req.query.customer as string),
   });
 });
 
 
 // Update payment information for an existing subscription
 app.get('/card', async (req, res) => {
-  const sub = await checkout.getSubscription(req.query.customer);
+  const sub = await checkout.getSubscription(req.query.customer as string);
   res.render('checkout', {
     checkout: {
       stripePublicKey: STRIPE_PUBLIC_KEY,
@@ -76,7 +76,7 @@ app.get('/card', async (req, res) => {
 
 app.post('/card', async (req, res) => {
   try {
-    await checkout.manageSubscription(req.query.customer, {
+    await checkout.manageSubscription(req.query.customer as string, {
       email: req.body.email,
       name: req.body.name,
       country: req.body.country,
@@ -93,7 +93,7 @@ app.post('/card', async (req, res) => {
 
 // Upgrade subscription for new customers
 app.get('/upgrade', async (req, res) => {
-  const sub = await checkout.getSubscription(req.query.customer);
+  const sub = await checkout.getSubscription(req.query.customer as string);
   res.render('checkout', {
     checkout: {
       stripePublicKey: STRIPE_PUBLIC_KEY,
@@ -110,8 +110,8 @@ app.get('/upgrade', async (req, res) => {
 
 app.post('/upgrade', async (req, res) => {
   try {
-    const stripeCustomerId = await checkout.manageSubscription(req.query.customer, {
-      plan: PREMIUM_PLAN_ID,
+    const stripeCustomerId = await checkout.manageSubscription(req.query.customer as string, {
+      plan: PLAN_ID,
       email: req.body.email,
       name: req.body.name,
       country: req.body.country,
@@ -128,13 +128,13 @@ app.post('/upgrade', async (req, res) => {
 
 
 app.get('/cancel', async (req, res) => {
-  await checkout.cancelSubscription(req.query.customer);
+  await checkout.cancelSubscription(req.query.customer as string);
   res.redirect('/?customer=' + req.query.customer);
 });
 
 
 app.get('/reactivate', async (req, res) => {
-  await checkout.reactivateSubscription(req.query.customer);
+  await checkout.reactivateSubscription(req.query.customer as string);
   res.redirect('/?customer=' + req.query.customer);
 });
 
